9c7b9e28b4b8e529ae66bc415e02da188524a8fd
thdwlgus <jiniluce7@naver.com>
2024-08-03 23:09:12 +0900

    7.1th

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,31 @@
-import React from "react";
+import React, { useState } from 'react'
+import { useHistory } from 'react-router-dom'
 
-export default function Login() {
-  return <div>Login</div>;
+function Login() {
+  const [username, setUsername] = useState('')
+  const history = useHistory()
+
+  const handleLogin = () => {
+    if (username) {
+      localStorage.setItem('loggedInUser', username)
+      history.push('/capital')
+    } else {
+      alert('Please enter your username')
+    }
+  }
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <input
+        type="text"
+        placeholder="Enter your username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  )
 }
+
+export default Login
