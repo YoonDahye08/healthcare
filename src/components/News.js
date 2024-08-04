d8a1ba6739a1828d4f5ca14961a4b41093f48b0d
leeazyone <leejiwon317@naver.com>
2024-08-04 21:36:26 +0900

    news&gugudan

--- src/components/News.js
+++ src/components/News.js
@@ -1,5 +1,23 @@
-import React from "react";
+import React from 'react'
+import newsData from './newsData'
 
-export default function News() {
-  return <div>News</div>;
+const News = () => {
+  return (
+    <div>
+      <h1>News</h1>
+      <ul>
+        {newsData.map((article, index) => (
+          <li key={index}>
+            <h2>{article.title}</h2>
+            <p>{article.description}</p>
+            <a href={article.url} target='_blank' rel='noopener noreferrer'>
+              사진으로 변경 후 사진 클릭하면 페이지로
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
 }
+
+export default News
